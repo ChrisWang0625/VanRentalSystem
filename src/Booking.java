6520708d4a696d042a39938545a2f86bb03da812
chrisoblivious <z5044589@ad.unsw.edu.au>
2017-03-28 12:16:07 +1100

    Create Booking and Vehicle class

---
+++ src/Booking.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ * Created by Jiuding on 2017/3/28.
+ */
+public class Booking {
+    private int id;
+    private ArrayList<Vehicle> vehicles;
+    private Date startDate;
+    private Date endDate;
+}
---
+++ src/Vehicle.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Jiuding on 2017/3/28.
+ */
+public class Vehicle {
+    private String name;
+    private String type;
+    private String location;
+}
